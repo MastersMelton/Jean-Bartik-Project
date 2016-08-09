2897f7b53e017a426493f5d12e49f5f9327146df
Kelly Masters-Melton <kmmgva@gmail.com>
2016-08-09 15:51:53 -0400

    Add files via upload

---
+++ codepen_xOyNoA/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("h1").addClass("animated pulse");
+   });
+$(document).ready(function(){
+  $("h2").addClass("animated fadein");
+});
+$(document).ready(function() {
+  $(".intro-background").addClass("animated fadeInUp");
+});
